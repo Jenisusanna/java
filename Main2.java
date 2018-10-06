f626afa0406e3638f7c8b10c056f66a7e7f919aa
Jenisusanna <43901688+Jenisusanna@users.noreply.github.com>
2018-10-06 14:44:25 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+    public static void main(String[] args) 
+	{
+
+        Scanner r = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int a = r.nextInt();
+
+        if(a>0&&a% 2 == 0)
+            System.out.println( "even");
+        else if(a%2!=0)
+            System.out.println("odd");
+            else 
+            System.out.println("invalid");
+    }
+}
