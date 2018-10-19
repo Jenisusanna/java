0ff4d6c15cedbf6f68c7da510181cfe5236a1600
Jenisusanna <43901688+Jenisusanna@users.noreply.github.com>
2018-10-19 06:14:48 -0700

    Add files via upload

---
+++ SumNatural.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class SumNatural
+ {
+
+    public static void main(String[] args) {
+
+        int num , sum = 0;
+ Scanner s=new Scanner(System.in);
+System.out.println(" ");
+ num = s.nextInt();
+
+
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
